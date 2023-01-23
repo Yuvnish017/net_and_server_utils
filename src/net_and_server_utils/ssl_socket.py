07af6c551c6f2c196014de9ba46004c3370a63e4
Yuvnish017 <malhotrayuvnish01@gmail.com>
2023-01-23 23:51:24 +0900

    "adding ssl socket server and client to the library"

--- src/net_and_server_utils/ssl_socket.py
+++ src/net_and_server_utils/ssl_socket.py
@@ -5,6 +5,16 @@ import socket
 class SSLSocketClient:
     def __init__(self):
         self.context = None
+        self.server_hostname = None
+        self.port = None
+        self.client_instance = None
 
     def context_creation(self):
         self.context = ssl.create_default_context()
+
+    def connect_to_server(self, server_hostname, port_number):
+        self.server_hostname = server_hostname
+        self.port = port_number
+        self.client_instance = self.context.wrap_socket(socket.socket(socket.AF_INET),
+                                                        server_hostname=self.server_hostname)
+        self.client_instance.connect((self.server_hostname, self.port))
--- tests/tests_ssl_socket.py
+++ tests/tests_ssl_socket.py
@@ -3,12 +3,21 @@ from network_utils.src.net_and_server_utils.ssl_socket import SSLSocketClient
 
 
 class TestSSLSocket(unittest.TestCase):
+    def setUp(self) -> None:
+        self.server_hostname = 'www.python.org'
+        self.port = 443
+
     def tests_client_class_import(self):
         SSLSocketClient()
 
     def tests_context_creation(self):
         curr_client = SSLSocketClient()
         curr_client.context_creation()
+
+    def tests_connect_to_server(self):
+        curr_client = SSLSocketClient()
+        curr_client.context_creation()
+        curr_client.connect_to_server(self.server_hostname, self.port)
 
 
 if __name__ == '__main__':
