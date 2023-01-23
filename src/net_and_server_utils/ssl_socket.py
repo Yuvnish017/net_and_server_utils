8b0a2edeacd10bcd4c3eebb183f64811a4134c80
Yuvnish017 <malhotrayuvnish01@gmail.com>
2023-01-24 00:18:51 +0900

    "adding ssl socket server and client to the library"

--- src/net_and_server_utils/ssl_socket.py
+++ src/net_and_server_utils/ssl_socket.py
@@ -8,6 +8,7 @@ class SSLSocketClient:
         self.server_hostname = None
         self.port = None
         self.client_instance = None
+        self.server_cert = None
 
     def context_creation(self):
         self.context = ssl.create_default_context()
@@ -18,3 +19,31 @@ class SSLSocketClient:
         self.client_instance = self.context.wrap_socket(socket.socket(socket.AF_INET),
                                                         server_hostname=self.server_hostname)
         self.client_instance.connect((self.server_hostname, self.port))
+
+    def get_server_certificate(self):
+        self.server_cert = self.client_instance.getpeercert()
+
+    def communicate(self):
+        while True:
+            client_input = input("Enter the data to be transmitted: ")
+            if client_input == 'disconnect':
+                print("disconnecting from the server...")
+                print(" ")
+                break
+            self.client_instance.sendall(client_input)
+            received_data = self.client_instance.recv(1024).split(b"\r\n")
+            print(f"Received data: {received_data}")
+
+    def communicate_and_save(self, filename):
+        while True:
+            client_input = input("Enter the data to be transmitted: ")
+            if client_input == 'disconnect':
+                print("disconnecting from the server....")
+                print(" ")
+                break
+            self.client_instance.sendall(client_input)
+            received_data = self.client_instance.recv(1024).split(b"\r\n")
+            print(f"Received data: {received_data}")
+            with open(filename, 'a') as file:
+                file.write(received_data)
+
--- tests/tests_ssl_socket.py
+++ tests/tests_ssl_socket.py
@@ -6,6 +6,7 @@ class TestSSLSocket(unittest.TestCase):
     def setUp(self) -> None:
         self.server_hostname = 'www.python.org'
         self.port = 443
+        self.response_file = 'socket_server_response.txt'
 
     def tests_client_class_import(self):
         SSLSocketClient()
@@ -18,6 +19,17 @@ class TestSSLSocket(unittest.TestCase):
         curr_client = SSLSocketClient()
         curr_client.context_creation()
         curr_client.connect_to_server(self.server_hostname, self.port)
+
+    def tests_communicate_and_save(self):
+        curr_client = SSLSocketClient()
+        curr_client.context_creation()
+        curr_client.connect_to_server(self.server_hostname, self.port)
+        curr_client.communicate_and_save(self.response_file)
+        count = 0
+        with open(self.response_file, 'r') as file:
+            for _ in file:
+                count += 1
+        self.assertEqual(count, 5)
 
 
 if __name__ == '__main__':
